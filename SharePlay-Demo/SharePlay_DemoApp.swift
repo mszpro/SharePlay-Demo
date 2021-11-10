9da17a55f167eeb99a7b38c88fc53d6cea841e95
Shunzhe <git@mszmagic.com>
2021-11-10 20:29:15 +0800

    Initial Commit

---
+++ SharePlay-Demo/SharePlay_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharePlay_DemoApp.swift
+//  SharePlay-Demo
+//
+//  Created by Shunzhe Ma on 2021/11/10.
+//
+
+import SwiftUI
+
+@main
+struct SharePlay_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
